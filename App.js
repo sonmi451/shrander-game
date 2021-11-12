af3a80beba26fdfcb11c21f873ef00d70c9c60c7
sonmi451 <aishamclean451@gmail.com>
2021-11-12 19:06:02 +0000

    update app

--- App.js
+++ App.js
@@ -47,27 +47,31 @@ export default class ShranderGame extends React.Component {
         () => console.log(this.state.shrander))
     }
 
-
     render() {
         const buttonAway = <Button color='orange' onPress={this.selectNone} title='It is too cute, take it away!'/>;
         const blank = <View/>;
 
         return (
             <View style={styles.container}>
-                <View style={styles.titleContainer}>
-                    <Text style={styles.titleText}>{this.state.title}</Text>
-                </View>
-                <View style={styles.buttonsContainer}>
-                    <Button color='orange' onPress={this.selectMolniya} title='Molniya'/>
-                    <Button color='orange' onPress={this.selectSemyorka} title='Semyorka'/>
-                    <Button color='orange' onPress={Math.random() < 0.5 ? this.selectMolniya : this.selectSemyorka} title='Give me a random one!'/>
-                </View>
-                <View style={styles.picContainer}>
-                    <Image style={styles.shranderPic} source={this.state.image} />
-                </View>
-                <View style={styles.buttonsContainer}>
-                    {this.state.shrander != 'none' ? buttonAway : null }
-                </View>
+
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>{this.state.title}</Text>
+            </View>
+
+            <View style={styles.oneButtonContainer}>
+                <Button color='orange' onPress={this.selectMolniya} title='Molniya'/>
+                <Button color='orange' onPress={this.selectSemyorka} title='Semyorka'/>
+            </View>
+
+            <View style={styles.oneButtonContainer}>
+                <Button color='orange' onPress={Math.random() < 0.5 ? this.selectMolniya : this.selectSemyorka} title='Give me a random one!'/>
+            </View>
+
+            <Image style={styles.shranderPic} source={this.state.image} />
+
+            <View style={styles.oneButtonContainer}>
+                {this.state.shrander != 'none' ? buttonAway : null }
+            </View>
             </View>
         );
     }
@@ -77,36 +81,36 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
+        justifyContent: 'space-between',
         backgroundColor: '#F5F5F5',
-        paddingTop: 90,
-        paddingBottom: 80,
-        paddingLeft: 50,
-        paddingRight: 50,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
     },
     titleContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'flex-end',
+        flex: 2,
+        fontSize: 26,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',
     },
     titleText: {
         fontSize: 26,
     },
-    buttonsContainer: {
+    oneButtonContainer: {
         flex: 1,
-        paddingLeft: 20,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
     },
     picContainer: {
-        flex: 2,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
     shranderPic: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        height:250,
-        width:250,
+        flex: 5,
+        alignSelf: 'center',
     },
 
 });
